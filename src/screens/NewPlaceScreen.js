25da98d785cdc77177f4b22ddb7df282dd8bd047
csabena <csabena@gmail.com>
2022-02-17 21:53:11 -0300

    first commit

---
+++ src/screens/NewPlaceScreen.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import { View, Text, StyleSheet, TextInput, ScrollView, Button } from 'react-native'
+import  { COLORS } from '../constants'
+import { useDispatch } from 'react-redux'
+import { addPlace } from '../store/places.actions'
+import ImageSelector from '../components/ImageSelector'
+const NewPlaceScreen = ({ navigation }) => {
+    const dispatch = useDispatch()
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+
+    const handleTitleChange = (text) =>  setTitle(text)
+
+    const handleSave = () => {
+        dispatch(addPlace(title, image))
+        navigation.navigate('Direcciones')
+    }
+
+    const handleOnImage = (uri) => {
+        setImage(uri)
+    }
+
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.label}>Titulo</Text>
+                <ImageSelector onImage={handleOnImage}/>
+                <TextInput 
+                    style={styles.input}
+                    onChangeText={handleTitleChange}
+                    value={title}
+                    />
+                <Button title="Grabar dirección" color={COLORS.MAROON} onPress={() => handleSave()} />
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    label: {
+        fontSize: 18,
+        marginVertical: 8,
+        color: '#212121',
+        fontWeight: 'bold'
+    },
+    input: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginVertical: 8,
+        padding: 4
+    }
+})
+
+export default NewPlaceScreen
---
+++ src/screens/PlaceListScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FlatList  } from 'react-native'
+import { useSelector } from 'react-redux'
+import PlaceItem from '../components/PlaceItem/index'
+
+const PlaceListScreen = ({ navigation }) => {
+    const places = useSelector(state => state.places.places)
+    console.warn(places)
+
+    const onSelectDetail = () => {
+        navigation.navigate('Detalle')
+    }
+
+    const renderItem = ({ item }) => (
+        <PlaceItem 
+            title={item.title}
+            image={item.image}
+            address='123 street, city, country'
+            onSelect={onSelectDetail}
+        />
+    )
+
+    return (
+        <FlatList
+            data={places}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+        />
+    )
+}
+
+export default PlaceListScreen
